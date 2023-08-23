f18cf975bb3ece41f4377943982cd0351e50661e
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-08-23 17:44:31 -0400

    began working on modal and form to upload book details, implemented preview image feature on the form, made several changes to html structure to facilitate new additions to library

--- src/script.js
+++ src/script.js
@@ -2,23 +2,49 @@
 
 const myLibrary = [];
 
-function Book() {}
+function Book(title, author, genre, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    return `${title} by ${author}, ${genre}, ${pages} pages, ${
+      read === true ? "read" : "have not read yet"
+    }`;
+  };
+}
+
+const uploadImg = document.getElementById("cover-img");
+uploadImg.addEventListener("change", function loadPreview() {
+  const previewImg = document.getElementById("preview-img");
+  previewImg.src = window.URL.createObjectURL(this.files[0]);
+  previewImg.onload(() => {
+    URL.revokeObjectURL(previewImg.src);
+  });
+});
+
+const addNewBook = document.querySelector(".add-new-book > div");
+addNewBook.addEventListener("click", addBookToLibrary);
 
 function addBookToLibrary() {}
 
 function displayBook() {}
 
-let covers = document.querySelectorAll(".cover");
+const covers = document.querySelectorAll(".bookDisplay");
 covers.forEach((cover) => {
   cover.addEventListener("click", collapseDetails);
 });
 
 function collapseDetails() {
-  this.classList.toggle("expand");
+  this.style.opacity = 0.7;
+  this.style.border = "5px solid var(--heading-text-color)";
   let content = this.nextElementSibling;
-  if (content.style.display==="grid") {
+  if (content.style.display === "grid") {
     content.style.display = "none";
+    this.style.opacity = 1;
+    this.style.border = "none";
   } else {
-    content.style.display = "grid"
+    content.style.display = "grid";
   }
 }
