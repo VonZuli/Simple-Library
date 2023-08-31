3dac7c5be672466cc807095cdb7f76eda9f7b3f7
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-08-30 22:02:02 -0400

    HTML/CSS[added name to form, changed form button to input with CSS adjustments.added toggle for read status(need to add functionality)/JS[addToLibrary logic complete,delete functionality WIP,book submit event listener for form complete]

--- src/script.js
+++ src/script.js
@@ -1,41 +1,54 @@
 "use strict";
+
 const myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Toliken",
-    genre: "Fiction/Fantasy",
-    year: 1937,
-    pages: 310,
-    read: true,
-  },
-  {
-    title: "Leviathan Wakes",
-    author: "James S. A. Corey",
-    genre: "Fiction/Sci-Fi",
-    year: 2011,
-    pages: 561,
-    read: false,
-  },
+  // {
+  //   title: "The Hobbit",
+  //   author: "J.R.R. Toliken",
+  //   genre: "Fiction/Fantasy",
+  //   year: 1937,
+  //   pages: 310,
+  //   read: true,
+  //   id: Date.now(),
+  // },
+  // {
+  //   title: "Leviathan Wakes",
+  //   author: "James S. A. Corey",
+  //   genre: "Fiction/Sci-Fi",
+  //   year: 2011,
+  //   pages: 561,
+  //   read: false,
+  //   id: Date.now(),
+  // },
 ];
 
 //book constructor
-function Book(title, author, genre, yearPublished, pages, read) {
+function Book(title, author, genre, yearPublished, pages, read, id) {
   this.title = title;
   this.author = author;
   this.genre = genre;
   this.yearPublished = yearPublished;
   this.pages = pages;
   this.read = read;
+  this.id = id;
 }
 
-const libraryDisplay = document.querySelector(".libraryDisplay");
-const bookCardContainer = document.getElementById("bookCardContainer");
-function addBookToLibrary() {
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  render();
+}
+
+function render() {
+  const bookCardContainer = document.getElementById("bookCardContainer");
+  let bookDetails = document.createElement("div");
+  bookDetails.setAttribute("class", "bookDetails");
+  bookDetails.innerHTML = "";
+
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
-    let bookDetails = document.createElement("div");
-    bookDetails.setAttribute("class", "bookDetails");
-
+    bookDetails.setAttribute("id", book.id);
     bookDetails.innerHTML = `
             <h2>${book.title}</h2>
             <h4>Author:</h4>
@@ -43,25 +56,49 @@ function addBookToLibrary() {
             <h4>Genre:</h4>
             <p>${book.genre}</p>
             <h4>Year:</h4>
-            <p>${book.year}</p>
+            <p>${book.yearPublished}</p>
             <h4>Pages:</h4>
             <p>${book.pages}</p>
             <h4>Read:</h4>
-            <p>${book.hasRead === true ? "Has read" : "Have not read yet"}</p>
-            <button id="deleteFromLibrary">Remove from Library</button>`;
+            <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+            <button class="deleteFromLibrary" id="${book.id}">Remove from Library</button>`;
 
     bookCardContainer.appendChild(bookDetails);
-    
-    const deleteFromLibrary = document.getElementById("deleteFromLibrary");
-    deleteFromLibrary.addEventListener('click', function deleteBook() {
-    })
   }
+  const deleteFromLibrary = document.querySelector(".deleteFromLibrary");
+  deleteFromLibrary.addEventListener("click", function deleteBook(e) {
+    console.log(bookDetails.id);
+    console.log(e.target.id);
+    console.log(bookDetails.id == e.target.id);
+    let index = myLibrary.indexOf(this.id);
+    console.log('index',index);
+    if (e.target.id == bookDetails.id) {
+      myLibrary.filter(( i => i !== index ));
+      render();
+    }
+  });
+}
+
+function addBookToLibrary() {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const genre = document.querySelector("#genre").value;
+  const yearPublished = document.querySelector("#yearPublished").value;
+  const pages = document.querySelector("#pages").value;
+  const hasRead = document.querySelector("#hasRead").checked;
+  const id = Date.now();
+
+  let newBook = new Book(title, author, genre, yearPublished, pages, hasRead, id);
+  myLibrary.push(newBook);
+  render();
 }
 
 const addNewBook = document.querySelector("#addBook");
-addNewBook.addEventListener("click", () => {
-  // e.preventDefault();
-  addBookToLibrary();
+addNewBook.addEventListener("click", (e) => {
+  e.preventDefault();
   const modal = document.getElementById("add-new-book_modal");
   const closeModal = document.querySelector(".close");
   modal.style.display = "block";
@@ -74,6 +111,17 @@ addNewBook.addEventListener("click", () => {
   };
 });
 
+const addBookSubmit = document.querySelector("#addBookSubmit");
+addBookSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  const modal = document.getElementById("add-new-book_modal");
+  modal.style.display = "none";
+  (() => {
+    const form = document.querySelector("form");
+    form.reset();
+  })();
+});
 // const updateBooksGrid = () => {
 //   resetBooksGrid()
 //   for (let book of myLibrary.books) {
